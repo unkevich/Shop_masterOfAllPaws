e1dee1873d7c6d9fc164452d72701dd65a5c80bb
Илья Ункевич <ilyaunkevitch@gmail.com>
2024-04-17 11:06:35 +0300

    Вкладка "Продать". Виджеты // v 0.0.8

--- cogs/windows.py
+++ cogs/windows.py
@@ -98,48 +98,22 @@ class my_window:
                 self.db_name = row[1]
                 self.db_price = row[2]
                 self.table_tov.insert('', END, values=[self.db_name, self.db_price])
-        elif table == self.table_tov:
+        elif table == self.table_buy:
             self.sql = self.new_connect.execute_sql(f'select T.name, T.price, TB.kol, TB.sum from tovar_buy TB inner join tovar T on TB.id_tovar = T.id')
             for row in self.sql:
                 self.db_name = row[0]
                 self.db_price = row[1]
                 self.db_kol = row[2]
                 self.db_sum = row[3]
-                self.table_tov.insert('', END, values=[self.db_name, self.db_price])
-
-    # вклад 'Товары'
-    def frame_tovar(self):
-        # таблица товаров
-        self.table_tov = Treeview(self.frame1, columns=['tovar', 'price'], show='headings')
-        self.table_tov.heading('tovar', text='Товар')
-        self.table_tov.heading('price', text='Цена')
-        self.table_tov.column('tovar', width=150, anchor='c')
-        self.table_tov.column('price', width=150, anchor='c')
-        self.table_tov.place(x=10, y=10)
-        # текст названия
-        self.tovar_name = StringVar()
-        self.lb_name = Label(self.frame1, text='Наименование товара:', background='lightblue', font='Arial 12')
-        self.lb_name.place(x=350, y=20)
-        # текстовое поля для ввода названия
-        self.entry_price = Entry(self.frame1, textvariable=self.tovar_name, font='Arial 12')
-        self.entry_price.place(x=350, y=60)
-        # текст цены
-        self.tovar_price = DoubleVar()
-        self.lb_name = Label(self.frame1, text='Цена товара:', background='lightblue', font='Arial 12')
-        self.lb_name.place(x=350, y=100)
-        # текстовое поля для ввода цены
-        self.entry_price = Entry(self.frame1, textvariable=self.tovar_price, font='Arial 12')
-        self.entry_price.place(x=350, y=140)
-        # кнопки
-        self.btn_new_tovar = Button(self.frame1, text='Добавить новый товар', command=self.create_tovar)
-        self.btn_new_tovar.place(x=600, y=60)
-        self.btn_del_tovar = Button(self.frame1, text='Удалить товар', command=self.del_tovar)
-        self.btn_del_tovar.place(x=600, y=100)
-        self.btn_update_tovar = Button(self.frame1, text='Изменить товар', command=self.update_tovar)
-        self.btn_update_tovar.place(x=600, y=140)
-
-        self.update_tables(self.table_tov)
-        self.table_tov.bind('<<TreeviewSelect>>', lambda event: self.select_tovar(self.tovar_name, self.tovar_price))
+                self.table_buy.insert('', END, values=[self.db_name, self.db_price])
+        elif table == self.table_sell:
+            self.sql = self.new_connect.execute_sql(f'select T.name, T.price, TS.kol, TS.sum from tovar_sell TS inner join tovar T on TS.id_tovar = T.id')
+            for row in self.sql:
+                self.db_name = row[0]
+                self.db_price = row[1]
+                self.db_kol = row[2]
+                self.db_sum = row[3]
+                self.table_sell.insert('', END, values=[self.db_name, self.db_price])
 
     def select_tovar(self, tovar_name, tovar_price):
         for row in self.table_tov.selection():
@@ -194,6 +168,78 @@ class my_window:
         else:
             messagebox.showerror('Ошибка', 'Товар не выбран!')
 
+    def buy(self):
+        if self.buy_tovar.get() != '' and self.sum > 0:
+            print(self.price)
+            self.new_connect = connect_db(name_db)
+            self.sql = self.new_connect.execute_sql(f"se;ect * from tovar where name = '{self.buy_tovar.get()}'")
+            for row in self.sql:
+                self.id_tovar = row[0]
+                self.new_connect.execute_sql(f"insert into tovar_buy (id_tovar, price, kol, sum) values ('{self.id_tovar}', '{self.price}', '{self.tovar_kol.get()}', '{self.sum}')")
+            self.new_connect.close_db()
+            self.update_tables(self.table_buy)
+        else:
+            messagebox.showerror('Ошибка', 'Вы не выбрали товар или не ввели количество')
+
+    def set_sum(self):
+        if self.tovar_kol.get() < 0:
+            messagebox.showerror('Ошибка', 'Количествло не может быть отрицательным!')
+            self.tovar_kol.set(0)
+        self.sum = self.tovar_kol.get()*self.price
+        self.lb_sum.configure(text=f'Сумма: {self.sum} руб.')
+
+    def combobox_tovar(self):
+        self.new_connect = connect_db(name_db)
+        self.sql = self.new_connect.execute_sql(f"select * from tovar where name='{self.buy_tovar.get()}'")
+
+        for row in self.sql:
+            self.price = row[2]
+        self.new_connect.close_db()
+        self.lb_price.configure(text=f'Цена: {self.price} руб.')
+        self.set_sum()
+
+    def kol_up(self):
+        self.tovar_kol.set(self.tovar_kol.get()+1)
+        self.set_sum()
+
+    def kol_down(self):
+        if self.tovar_kol.get() > 0:
+            self.tovar_kol.set(self.tovar_kol.get()-1)
+            self.set_sum()
+
+    # вклад 'Товары'
+    def frame_tovar(self):
+        # таблица товаров
+        self.table_tov = Treeview(self.frame1, columns=['tovar', 'price'], show='headings')
+        self.table_tov.heading('tovar', text='Товар')
+        self.table_tov.heading('price', text='Цена')
+        self.table_tov.column('tovar', width=150, anchor='c')
+        self.table_tov.column('price', width=150, anchor='c')
+        self.table_tov.place(x=10, y=10)
+        # текст названия
+        self.tovar_name = StringVar()
+        self.lb_name = Label(self.frame1, text='Наименование товара:', background='lightblue', font='Arial 12')
+        self.lb_name.place(x=350, y=20)
+        # текстовое поля для ввода названия
+        self.entry_price = Entry(self.frame1, textvariable=self.tovar_name, font='Arial 12')
+        self.entry_price.place(x=350, y=60)
+        # текст цены
+        self.tovar_price = DoubleVar()
+        self.lb_name = Label(self.frame1, text='Цена товара:', background='lightblue', font='Arial 12')
+        self.lb_name.place(x=350, y=100)
+        # текстовое поля для ввода цены
+        self.entry_price = Entry(self.frame1, textvariable=self.tovar_price, font='Arial 12')
+        self.entry_price.place(x=350, y=140)
+        # кнопки
+        self.btn_new_tovar = Button(self.frame1, text='Добавить новый товар', command=self.create_tovar)
+        self.btn_new_tovar.place(x=600, y=60)
+        self.btn_del_tovar = Button(self.frame1, text='Удалить товар', command=self.del_tovar)
+        self.btn_del_tovar.place(x=600, y=100)
+        self.btn_update_tovar = Button(self.frame1, text='Изменить товар', command=self.update_tovar)
+        self.btn_update_tovar.place(x=600, y=140)
+
+        self.update_tables(self.table_tov)
+        self.table_tov.bind('<<TreeviewSelect>>', lambda event: self.select_tovar(self.tovar_name, self.tovar_price))
 
     # вкладка 'Купить'
     def frame_buy(self):
@@ -246,43 +292,57 @@ class my_window:
 
     # вкладка 'Продать'
     def frame_sell(self):
-        pass
-    
-    def kol_up(self):
-        self.tovar_kol.set(self.tovar_kol.get()+1)
-        self.set_sum()
+        self.table_sell = Treeview(self.frame3, columns=['tovar', 'price', 'kol', 'sum'], show='headings')
+        self.table_sell.heading('tovar', text='Товар')
+        self.table_sell.heading('price', text='Цена')
+        self.table_sell.heading('kol', text='Количество')
+        self.table_sell.heading('sum', text='Сумма')
 
-    def kol_down(self):
-        if self.tovar_kol.get() > 0:
-            self.tovar_kol.set(self.tovar_kol.get()-1)
-            self.set_sum()
+        self.table_sell.column('tovar', width=150, anchor='c')
+        self.table_sell.column('price', width=150, anchor='c')
+        self.table_sell.column('kol', width=150, anchor='c')
+        self.table_sell.column('sum', width=150, anchor='c')
 
-    def buy(self):
-        if self.buy_tovar.get() != '' and self.sum > 0:
-            print(self.price)
-            self.new_connect = connect_db(name_db)
-            self.sql = self.new_connect.execute_sql(f"se;ect * from tovar where name = '{self.buy_tovar.get()}'")
-            for row in self.sql:
-                self.id_tovar = row[0]
-                self.new_connect.execute_sql(f"insert into tovar_buy (id_tovar, price, kol, sum) values ('{self.id_tovar}', '{self.price}', '{self.tovar_kol.get()}', '{self.sum}')")
-            self.new_connect.close_db()
-            self.update_tables(self.table_buy)
-        else:
-            messagebox.showerror('Ошибка', 'Вы не выбрали товар или не ввели количество')
+        self.update_tables(self.table_sell)
 
-    def set_sum(self):
-        if self.tovar_kol.get() < 0:
-            messagebox.showerror('Ошибка', 'Количествло не может быть отрицательным!')
-            self.tovar_kol.set(0)
-        self.sum = self.tovar_kol.get()*self.price
-        self.lb_sum.configure(text=f'Сумма: {self.sum} руб.')
-
-    def combobox_tovar(self):
+        self.tovar_sell_list = []
         self.new_connect = connect_db(name_db)
-        self.sql = self.new_connect.execute_sql(f"select * from tovar where name='{self.buy_tovar.get()}'")
-
+        self.sql = self.new_connect.execute_sql(f"select * from tovar")
         for row in self.sql:
-            self.price = row[2]
+            self.tovar_sell_list.append(row[1])
         self.new_connect.close_db()
-        self.lb_price.configure(text=f'Цена: {self.price} руб.')
-        self.set_sum()
+
+        self.lb_name = Label(self.frame3, text='Выберите товар:', font='Arial 12', background='lightblue')
+        self.lb_name.place(x=10, y=250)
+        self.sell_tovar = Combobox(self.frame3, values=self.tovar_sell_list, state='readonly')
+        self.sell_tovar.place(x=10, y=300)
+
+        self.lb_price2 = Label(self.frame3, text='Установите наценку:', font='Arial 12', background='lightblue')
+        self.lb_price2.place(x=170, y=250)
+        self.price2_tovar = Combobox(self.frame3, values=['10%', '15%', '20%', '25%', '30%'], state='readonly')
+        self.price2_tovar.place(x=170, y=300, width=50)
+        self.price2_tovar.current(0)
+        self.price2_tovar.place(x=170, y=300, width=50)
+
+        self.sell_tovar_kol = IntVar()
+        self.lb_kol = Label(self.frame3, text='Введите количество:', font='Arial 12', background='lightblue')
+        self.lb_kol.place(x=200, y=250)
+        self.sell_kol = Entry(self.frame3, textvariable=self.tovar_kol)
+        self.sell_kol.place(x=350, y=300, width=100)
+
+        self.btn_up = Button(self.frame3, text='🔺',)
+        self.btn_up.place(x=450, y=300, width=25, height=23)
+        self.btn_down = Button(self.frame3, text='🔻',)
+        self.btn_down.place(x=475, y=300, width=25, height=23)
+
+        self.lb_sell_price = Label(self.frame3, text='Итого: 0 руб.', font='Arial 12', background='lightblue')
+        self.lb_sell_price.place(x=10, y=340)
+
+        self.lb_sell_sum = Label(self.frame3, text='Итого: 0 руб.', font='Arial 12', background='lightblue')
+        self.lb_sell_sum.place(x=350, y=340)
+
+        self.lb_kol_info = Label(self.frame3, text='Товаров на складе: 0.', font='Arial 12', background='lightblue')
+        self.lb_kol_info.place(x=650, y=50)
+
+        self.btn_send_sell = Button(self.frame3, text='Продать')
+        self.btn_send_sell.place(x=550, y=300)
